6d41a5e625aa0c3525efd0da1b8c93b1c8cb4baf
Javier Rivera <Sunnuke@users.noreply.github.com>
2020-12-08 13:13:59 -0800

    Session count is functional, adding reset count route

--- counter/src/main/java/com/practice/counter/ControllerCounter.java
+++ counter/src/main/java/com/practice/counter/ControllerCounter.java
@@ -1,4 +1,4 @@
-package com.controllers;
+package com.practice.counter;
 
 import javax.servlet.http.HttpSession;
 
@@ -6,17 +6,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
-@RequestMapping("/practice/")
+@RequestMapping("/practice")
 public class ControllerCounter {
 	
-	@RequestMapping("")
+	@RequestMapping("/")
 	public String counter(HttpSession session) {
 		if (session.getAttribute("count") == null) {
 			session.setAttribute("count", 0);
 		}
-		else {
-			int count = (int) session.getAttribute("count");
+		if (session.getAttribute("count") != null) {
+			Integer count = (Integer) session.getAttribute("count");
 			count++;
 			session.setAttribute("count", count);
 		}
@@ -24,16 +25,23 @@ public class ControllerCounter {
 		
 	}
 	
-	@RequestMapping("counter")
+	@RequestMapping("/counter")
 	public String count(HttpSession session, Model model) {
 		if (session.getAttribute("count") == null) {
 			model.addAttribute("count", 0);
 		}
-		else {
-			int count = (int) session.getAttribute("count");
+		if (session.getAttribute("count") != null) {
+			Integer count = (Integer) session.getAttribute("count");
 			model.addAttribute("count", count);
 		}
 		return "counter.jsp";
+		
+	}
+	
+	@RequestMapping("/reset")
+	public String reset(HttpSession session, Model model) {
+		session.setAttribute("count", 0);
+		return "/practice/counter";
 		
 	}
 
